363cf24b1987292d4a787264346bae46cdd0de95
MatteoMolteni08 <toadteo08@gmail.com>
2025-11-14 16:28:19 +0100

    Update Matrice.java

--- Capitolo 5/MatrixSorter/src/Matrice.java
+++ Capitolo 5/MatrixSorter/src/Matrice.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class Matrice {
-    private double[][] matrice;
+    private int[][] matrice;
     private int colonne;
     private int righe;
+    Random random = new Random();
 
     public int getColonne() {
         return colonne;
@@ -14,11 +17,11 @@ public class Matrice {
     public Matrice(int colonne, int righe) {
         this.colonne = colonne;
         this.righe = righe;
-        this.matrice = new double[righe][colonne];
+        this.matrice = new int[righe][colonne];
     }
 
     public Matrice(){
-        this.matrice = new double[5][5];
+        this.matrice = new int[5][5];
     }
     public void stampaMatrice(){
         for (int i = 0; i < matrice.length; i++) {
@@ -36,8 +39,10 @@ public class Matrice {
     public void popolaMatrice(){
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
-                matrice[i][j] = Math.random();
+                matrice[i][j] = random.nextInt(1,10);
             }
         }
     }
+
+
 }
