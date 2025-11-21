09bd9d3c6f4295a03f131b835da17eb776e30b2f
MatteoMolteni08 <toadteo08@gmail.com>
2025-11-21 16:28:08 +0100

    Update Room.java

--- Capitolo6/GestioneImmobiliare/src/Room.java
+++ Capitolo6/GestioneImmobiliare/src/Room.java
@@ -1,3 +1,28 @@
 public class Room {
+    private String nome // es. "Cucina", "Camera Matrimoniale"
+    private double superficieMq // es. 15.5
 
+    public Room(String nome, double superficieMq) {
+        this.nome = nome;
+        this.superficieMq = superficieMq;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSuperficieMq() {
+        return superficieMq;
+    }
+    public void setSuperficieMq(double superficieMq) {
+        this.superficieMq = superficieMq;
+    }
+
+    @Override
+    public String toString() {
+        return nome + ' (' + superficieMq +
+                ' mq)';
+    }
 }
