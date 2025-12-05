d80abb7c2a41281df6ac585ef081a5c12f5642c3
MatteoMolteni08 <toadteo08@gmail.com>
2025-12-05 16:26:26 +0100

    Update Persona.java

--- Capitolo7/HelloPersona/src/Persona.java
+++ Capitolo7/HelloPersona/src/Persona.java
@@ -1,10 +1,14 @@
+import java.util.Objects;
+
 public class Persona {
     private String nome;
     private int anni;
+    private String avs;
 
-    public Persona(String nome, int anni) {
+    public Persona(String nome, int anni, String avs) {
         this.nome = nome;
         this.anni = anni;
+        this.avs = avs;
     }
 
     public String getNome() {
@@ -19,6 +23,31 @@ public class Persona {
     public void setAnni(int anni) {
         this.anni = anni;
     }
+    public String getAvs() {
+        return avs;
+    }
+    public void setAvs(String avs) {
+        this.avs = avs;
+    }
 
-    
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nome='" + nome + '\'' +
+                ", anni=" + anni +
+                ", avs='" + avs + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return anni == persona.anni && Objects.equals(nome, persona.nome) && Objects.equals(avs, persona.avs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, anni, avs);
+    }
 }
