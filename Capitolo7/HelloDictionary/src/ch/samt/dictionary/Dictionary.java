bc8761ba23d1db85cd205fb1888e261fc86725ff
MatteoMolteni08 <toadteo08@gmail.com>
2025-12-05 15:43:26 +0100

    create HelloDictionary
    HELLODICTIONARY exist NOW

---
+++ Capitolo7/HelloDictionary/src/ch/samt/dictionary/Dictionary.java
@@ -0,0 +1,24 @@
+package ch.samt.dictionary;
+
+import java.util.HashMap;
+
+public class Dictionary {
+    private HashMap<String, Entry> vocaboli;
+
+    public Dictionary() {
+        vocaboli = new HashMap<>();
+    }
+    public void aggiungi(Entry e) {
+        vocaboli.put(e.getParolaItaliano(), e);
+    }
+
+    public Entry cerca(String parolaItaliano) {
+        return vocaboli.get(parolaItaliano);
+    }
+
+    public void stampaTutto(){
+        for(Entry v : vocaboli.values()){
+            System.out.println(v);
+        }
+    }
+}
---
+++ Capitolo7/HelloDictionary/src/ch/samt/main/App.java
@@ -0,0 +1,7 @@
+package ch.samt.main;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
