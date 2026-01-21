1ee31e0aaea6f88463235ed39c61b69d9b4cda5a
MatteoMolteni08 <toadteo08@gmail.com>
2026-01-21 15:05:19 +0100

    create SmartElettricGarage

---
+++ Capitolo9/SmartElettricGarage/src/ElectricVehicle.java
@@ -0,0 +1,33 @@
+public abstract class ElectricVehicle {
+    private String model;
+    private int batteryLevel;
+
+    public ElectricVehicle(String model, int batteryLevel) {
+        this.model = model;
+        this.batteryLevel = batteryLevel;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public int charge(int amount) {
+        if (amount < 0) {
+            return batteryLevel;
+        }else if (batteryLevel + amount > 100) {
+            batteryLevel = 100;
+        }else {
+            batteryLevel += amount;
+        }
+        return batteryLevel;
+    }
+}
